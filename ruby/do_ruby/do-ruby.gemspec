8dbc383fc29854986af0b28455eaad23d308ed52
Jason Jones <jj@bizzle.(none)>
2008-11-17 01:38:45 -0900

    Refactored into gnomedo-ruby.

--- ruby/do_ruby/do-ruby.gemspec
+++ ruby/do_ruby/do-ruby.gemspec
@@ -1,6 +1,8 @@
 Gem::Specification.new do |s|
    s.name = %q{do-ruby}
    s.version = "0.0.1"
+   
+   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
    s.date = %q{2008-11-16}
    s.authors = ["Jason Jones"]
    s.email = %q{jasonedwardjones@gmail.com}
--- ruby/tinyurl_action.rb
+++ ruby/tinyurl_action.rb
@@ -1,24 +1,22 @@
 #!/usr/bin/env ruby
-%w{rubygems json trollop net/http do_ruby}.each { |lib| require lib}
-include DoRuby
-
+%w{rubygems json trollop net/http gnomedo-ruby}.each { |lib| require lib}
 class TinyUrlAction
   attr_accessor :action_definition
   
   def initialize()
-    @action_definition = ActionDefinition.new :name => "Send to TinyUrl", :description => "Shortens a URL using TinyUrl"
-    @return_items = []
+    @action_definition = GnomeDo::Action.new :name => "Send to TinyUrl", 
+                                             :description => "Shortens a URL using TinyUrl"
   end
   
   def print_action_definition
-    puts @action_definition.to_json
+   puts @action_definition.to_json
   end
   
   def get_shortened_url_item(url)
     http = Net::HTTP.start("tinyurl.com", 80)
     response = http.get("/api-create.php?url=#{url}")
     tinyurl = response.read_body
-    url_item = TextItem.new :name => "TinyUrl Item", :description => url, :text => tinyurl
+    url_item = GnomeDo::TextItem.new :name => "TinyUrl Item", :description => url, :text => tinyurl
   end
   
   def get_urls_from_json(json)
